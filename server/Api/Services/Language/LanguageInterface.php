af8bdf3965965f79046e480f70f0a220d99c34c3
Romchik38 <pomahehko.c@gmail.com>
2024-09-04 12:46:10 +0300

    language service is ready

--- server/Api/Services/Language/LanguageInterface.php
+++ server/Api/Services/Language/LanguageInterface.php
@@ -2,13 +2,23 @@
 
 declare(strict_types=1);
 
-namespace Romchik38\Server\Services\Language;
+namespace Romchik38\Server\Api\Services\Language;
+
+use Romchik38\Server\Api\Models\DTO\Language\LanguageDTOInterface;
 
 interface LanguageInterface
 {
     const DEFAULT_LANGUAGE_FIELD = 'default_language';
     const LANGUAGE_LIST_FIELD = 'language_list';
 
-    public function getDefaultLanguage(): string;
+    /** return language entity */
+    public function getDefaultLanguage(): LanguageDTOInterface;
+
+    /** 
+     * return a list of language entities
+     * see list in the config file
+     * 
+     * @return LanguageDTOInterface[]
+     */
     public function getLanguageList(): array;
 }
