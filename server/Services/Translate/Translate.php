f6210b37515d42c8def5837198b3b45f7e8d9689
Romchik38 <pomahehko.c@gmail.com>
2024-09-05 15:37:51 +0300

    translate in progress

---
+++ server/Services/Translate/Translate.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Server\Services\Translate;
+
+// $hash = [
+//     'login.index.h1' => [
+//         'en' => 'Hello world',
+//         'uk' => 'Привіт світ'
+//     ]
+// ];
+
+// $__ = new A($hash, 'en', 'uk');
+
+// echo $__('login.index.h1');
+
+
+class Translate
+{
+    public function __construct(
+        protected readonly array $hash,
+        protected readonly string $defaultLang,
+        protected readonly string $currentLang
+    ) {}
+
+    public function __invoke(string $str)
+    {
+        $item = $this->hash[$str] ??
+            throw new \RuntimeException('invalid trans string');
+        $defaultVal = $item[$this->defaultLang] ??
+            throw new \RuntimeException('default value for lang ' . $this->defaultLang . ' isn\'t set');
+        return $item[$this->currentLang] ?? $defaultVal;
+    }
+}
