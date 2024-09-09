b0b4ba4e87d1f45e8d1921d48b1aa4c3bd6e6615
Romchik38 <pomahehko.c@gmail.com>
2024-09-09 15:58:52 +0300

    test for TranslateStorage is started

---
+++ tests/unit/Services/Translate/TranslateStorageTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Romchik38\Server\Services\Translate\TranslateStorage;
+
+class TranslateStorageTest extends TestCase {
+    
+}
