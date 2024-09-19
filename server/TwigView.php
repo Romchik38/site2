b651fdb326e049bc14fe108bff0c66810f1e072a
Romchik38 <pomahehko.c@gmail.com>
2024-09-19 19:38:11 +0300

    twig view in progress

---
+++ server/TwigView.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Server\Views\Http;
+
+use Romchik38\Server\Api\Controllers\ControllerInterface;
+use Romchik38\Server\Api\Models\DTO\DefaultView\DefaultViewDTOInterface;
+use \Romchik38\Server\Api\Views\Http\HttpViewInterface;
+
+class TwigView implements HttpViewInterface
+{
+    protected DefaultViewDTOInterface $controllerData;
+    protected array $metaData = [];
+    protected ControllerInterface|null $controller = null;
+    protected string $action;
+
+    public function __construct() {}
+
+    public function setController(ControllerInterface $controller, string $action = ''): HttpViewInterface
+    {
+        $this->controller = $controller;
+        $this->action = $action;
+        return $this;
+    }
+
+    public function setControllerData(DefaultViewDTOInterface $data): HttpViewInterface
+    {
+        $this->controllerData = $data;
+        return $this;
+    }
+
+    public function setMetadata(string $key, string $value): HttpViewInterface
+    {
+        $this->metaData[$key] = $value;
+        return $this;
+    }
+
+    public function toString(): string
+    {
+        $this->prepareMetaData($this->controllerData);
+        return $this->build();
+    }
+
+    protected function build(): string
+    {
+
+        $html = '';
+
+        return $html;
+    }
+
+    protected function prepareMetaData(DefaultViewDTOInterface $data): void
+    {
+        /** use this for add info to metaData */
+    }
+}
