8a06462fba5a65016c3fd1825a95e82967513289
Romchik38 <pomahehko.c@gmail.com>
2024-09-30 15:18:31 +0300

    added 1 test

--- tests/unit/TwigViewTest.php
+++ tests/unit/TwigViewTest.php
@@ -6,17 +6,18 @@ use Romchik38\Site2\Views\Html\TwigView;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 use Romchik38\Server\Services\Translate\Translate;
-use Romchik38\Server\Services\Translate\TranslateStorage;
 use PHPUnit\Framework\TestCase;
+use Romchik38\Server\Views\Http\Errors\ViewBuildException;
 
 class TwigViewTest extends TestCase {
     public function testConstruct(){
         $loader = new FilesystemLoader();
         $twigEnv = new Environment($loader);
         
-        
         $translate = $this->createMock(Translate::class);
+
+        $this->expectException(ViewBuildException::class);
         $view = new TwigView($twigEnv, null, $translate);
-        
+        $view->toString();
     }
 }
