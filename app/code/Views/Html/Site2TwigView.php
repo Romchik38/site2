01aa427acbbe9cab478bbefa6e320f982bb49a9d
Romchik38 <pomahehko.c@gmail.com>
2024-10-11 22:16:57 +0300

    update tasks

--- app/code/Views/Html/Site2TwigView.php
+++ app/code/Views/Html/Site2TwigView.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 namespace Romchik38\Site2\Views\Html;
 
-use Romchik38\Server\Api\Models\DTO\DefaultView\DefaultViewDTOInterface;
-
 final class Site2TwigView extends TwigView {
     
     protected function prepareMetaData(): void
     {
-
-        /** 
-         *   1. add to array $this->metaData key/value
-         *   2. meta_data will be avalible in a template
-         * */
+        $this->setMetadata('language', $this->dynamicRootService->getCurrentRoot()->getName());   
     }
 }
