e1b99b5475be29d8e69818a92340ee30831dc1ad
Romchik38 <pomahehko.c@gmail.com>
2024-10-17 21:20:05 +0300

    language selector works

--- public/http/media/js/language.js
+++ public/http/media/js/language.js
@@ -7,18 +7,7 @@
 
 (() => {
 
-    //alert('before DOMContentLoaded');
-
     addEventListener("DOMContentLoaded", (event) => {
-        //alert('after DOMContentLoaded');
-        //temp
-        var fon = document.getElementsByClassName('background-secondary')[0];
-        // fon.addEventListener('click', (e) => {
-        //     console.log(e.type);
-        //     //alert(e.type);
-        //     fon.style.backgroundColor = '#eee';
-        // });
-        
 
         var className = 'language';
         var elems = document.getElementsByClassName(className);
@@ -27,24 +16,15 @@
             console.error('userinfo script started, but some of the elements not found on the page, pls check it: [header-user-notloggedin, header-user-loggedin, user-name-field]');
             return;
         }
-        //alert('before for');
+
         for (var i = 0; i < elems.length; i++) {
             var elem = elems[i];
             
-            elem.disabled = false;
-            
-
-            elem.addEventListener('click', (e) => {
-                
-                // alert('click before if');
-                if (e.target.value.length !== 0) {
-                    fon.style.backgroundColor = '#fff';
-                    //alert('inside if');
-                    //alert(e.target.value);
-                    //window.location.href = e.target.value;
-                    //document.location.assign(e.target.value)
-                }
+            elem.addEventListener('change', (e) => {
+                window.location.href = e.target.value;
             });
+
+            elem.disabled = false;
         }
         
     });
