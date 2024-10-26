27d61fc6487395a332042d9b7ed228ca685a7ead
Romchik38 <pomahehko.c@gmail.com>
2024-10-26 13:31:26 +0300

    updated

--- sql/article.sql
+++ sql/article.sql
@@ -1,14 +1,15 @@
 CREATE table
     article (
-        identifier text UNIQUE NOT NULL
+        identifier text UNIQUE NOT NULL,
+        active boolean DEFAULT false
     );
 
 CREATE table
     article_translates (
-        article_id text NOT NULL REFERENCES article (identifier) ON DELETE CASCADE,
-        language text NOT NULL REFERENCES translate_lang (language) ON DELETE CASCADE,
-        name text NOT NULL UNIQUE,
-        description text NOT NULL,
+        article_id text NOT NULL REFERENCES article (identifier) ON DELETE CASCADE ON UPDATE CASCADE,
+        language text NOT NULL REFERENCES translate_lang (language) ON DELETE CASCADE ON UPDATE CASCADE,
+        name text UNIQUE,
+        description text,
         created_at timestamp NOT NULL DEFAULT current_timestamp,
         updated_at timestamp NOT NULL DEFAULT current_timestamp,
         CONSTRAINT pk_article_translates PRIMARY KEY (article_id, language)
@@ -16,8 +17,8 @@ CREATE table
 
 CREATE table
     article_category (
-        article_id text NOT NULL REFERENCES article (identifier) ON DELETE CASCADE,
-        category_id text NOT NULL REFERENCES category (identifier) ON DELETE CASCADE,
+        article_id text NOT NULL REFERENCES article (identifier) ON DELETE CASCADE ON UPDATE CASCADE,
+        category_id text NOT NULL REFERENCES category (identifier) ON DELETE CASCADE ON UPDATE CASCADE,
         CONSTRAINT pk_article_category PRIMARY KEY (article_id, category_id)
     );
 
