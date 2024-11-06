68aa92739ca04ef474d66d13dfa31a19fd8b07d3
Romchik38 <pomahehko.c@gmail.com>
2024-11-06 12:24:40 +0200

    refactor

--- app/bootstrap.back/domain_service.php
+++ app/bootstrap.back/domain_service.php
@@ -4,31 +4,10 @@ declare(strict_types=1);
 
 return function ($container) {
     
-    // Article
-    $container->add(
-        \Romchik38\Site2\Domain\Article\Services\ArticleListService::class,
-        new \Romchik38\Site2\Domain\Article\Services\ArticleListService(
-            $container->get(\Romchik38\Site2\Domain\Api\Article\ArticleRepositoryInterface::class),
-            $container->get(\Romchik38\Server\Api\Models\SearchCriteria\SearchCriteriaFactoryInterface::class),
-            $container->get(\Romchik38\Site2\Domain\Api\Article\ArticleFilterFactoryInterface::class)
-        )
-    );
 
-    $container->add(
-        \Romchik38\Site2\Domain\Article\View\ArticleDTOFactory::class,
-        new \Romchik38\Site2\Domain\Article\View\ArticleDTOFactory(
-            $container->get(\Romchik38\Site2\Api\Services\DateFormatterInterface::class),
-            $container->get(\Romchik38\Site2\Api\Services\ReadLengthFormatterInterface::class)
-        )
-    );
 
-    $container->add(
-        \Romchik38\Site2\Domain\Article\View\ArticleViewRepository::class,
-        new \Romchik38\Site2\Domain\Article\View\ArticleViewRepository(
-            $container->get(\Romchik38\Site2\Domain\Api\Article\ArticleRepositoryInterface::class),
-            $container->get(\Romchik38\Site2\Domain\Article\View\ArticleDTOFactory::class)
-        )
-    );
+
+
 
     return $container;
 };
