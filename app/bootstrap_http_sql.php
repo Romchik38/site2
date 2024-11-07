7339f9dc5e64a63188323596df10b99c1cbfa436
Romchik38 <pomahehko.c@gmail.com>
2024-11-07 13:16:57 +0200

    bootstrap file is ready

--- app/bootstrap_http_sql.php
+++ app/bootstrap_http_sql.php
@@ -7,6 +7,52 @@ use Romchik38\Container;
 return function () {
     $container = new Container();
     
+    $no_dependence = require_once(__DIR__ . '/bootstrap/no_dependence.php');
+    $no_dependence($container);
+
+    $sql_no_dependence = require_once(__DIR__ . '/bootstrap/sql/no_dependencies.php');
+    $sql_no_dependence($container);
+
+    $sql_models = require_once(__DIR__ . '/bootstrap/sql/models.php');
+    $sql_models($container);
+
+    $services_before_application = require_once(__DIR__ . '/bootstrap/services_before_application.php');
+    $services_before_application($container);
+
+    $application = require_once(__DIR__ . '/bootstrap/application.php');
+    $application($container); 
+
+    $services_after_application = require_once(__DIR__ . '/bootstrap/services_after_application.php');
+    $services_after_application($container);
+
+    $services = require_once(__DIR__ . '/bootstrap/services.php');
+    $services($container);
+  
+    $http_no_dependence = require_once(__DIR__ . '/bootstrap/http/no_dependence.php');
+    $http_no_dependence($container);
+
+    $http_services = require_once(__DIR__ . '/bootstrap/http/services.php');
+    $http_services($container);
+
+    $http_headers = require_once(__DIR__ . '/bootstrap/http/headers.php');
+    $http_headers($container);    
+
+    $http_views = require_once(__DIR__ . '/bootstrap/http/views.php');
+    $http_views($container);
+
+    $actions = require_once(__DIR__  . '/bootstrap/actions.php');
+    $actions($container);
+
+    $actionList = require_once(__DIR__  . '/bootstrap/http/actionsList.php');
+    $actionList($container);
+
+    // ROUTER
+    $router = require_once(__DIR__ . '/bootstrap/http/router.php');
+    $router($container);
+
+    // SERVER
+    $server = require_once(__DIR__ . '/bootstrap/http/server.php');
+    $server($container);
 
     return $container;
 };
--- public/http/index.php
+++ public/http/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once(__DIR__ . '/../../vendor/autoload.php');
 
-$container = (require_once(__DIR__ . './../../app/bootstrap_http.php'))();
+$container = (require_once(__DIR__ . './../../app/bootstrap_http_sql.php'))();
 
 /** @var HttpServerInterface $server */
 $server = $container->get(\Romchik38\Server\Api\Servers\Http\HttpServerInterface::class);
