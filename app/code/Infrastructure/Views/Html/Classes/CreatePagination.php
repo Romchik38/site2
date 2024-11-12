7a91f7dee6f1e229cae77d8da2332bee4594687a
Romchik38 <pomahehko.c@gmail.com>
2024-11-12 20:43:51 +0200

    pagination in progress

--- app/code/Infrastructure/Views/Html/Classes/CreatePagination.php
+++ app/code/Infrastructure/Views/Html/Classes/CreatePagination.php
@@ -6,6 +6,7 @@ namespace Romchik38\Site2\Infrastructure\Views\Html\Classes;
 
 use Romchik38\Site2\Infrastructure\Views\CreatePaginationInterface;
 
+/** It is responsable for creating HTML pagination block */
 final class CreatePagination implements CreatePaginationInterface
 {
     public static function createHtml(
@@ -15,7 +16,22 @@ final class CreatePagination implements CreatePaginationInterface
         int $displayed
     ): string {
 
+        // Case 1: do not show pagination
+        if($displayed = $totalCount) return '';
+        
+        // Case 2: show pagination
+               
+        $firstElement = '';
+        $middle = '';
+        $last = '';
+        // Curent page goes first
+        if($page === 1) {
+            $firstElement = '<div>1</div>';
+        }
+
         $remainder = $totalCount - $displayed;
+
+        $elements = [$firstElement, $middle, $last]; 
 
         return <<<HTML
         <div>Displayed: {$displayed} </div>
