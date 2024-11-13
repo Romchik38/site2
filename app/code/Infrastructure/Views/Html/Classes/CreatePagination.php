20c33db17efcabf99785e76db686dc73e56a8899
Romchik38 <pomahehko.c@gmail.com>
2024-11-13 12:39:38 +0200

    pagination first in progress

--- app/code/Infrastructure/Views/Html/Classes/CreatePagination.php
+++ app/code/Infrastructure/Views/Html/Classes/CreatePagination.php
@@ -9,6 +9,7 @@ use Romchik38\Site2\Infrastructure\Views\CreatePaginationInterface;
 /** It is responsable for creating HTML pagination block */
 final class CreatePagination implements CreatePaginationInterface
 {
+    
     public static function createHtml(
         int $page,
         int $limit,
@@ -17,27 +18,39 @@ final class CreatePagination implements CreatePaginationInterface
     ): string {
 
         // Case 1: do not show pagination
-        if($displayed = $totalCount) return '';
+        if($displayed === $totalCount) return '';
         
-        // Case 2: show pagination
-               
+        // Case 2: show pagination       
         $firstElement = '';
         $middle = '';
         $last = '';
-        // Curent page goes first
+        $dots = '<div>...</div>';
+        $remainder = $totalCount - $displayed;
+        $maxNextPage = 2;
+
+        // FIRST
         if($page === 1) {
             $firstElement = '<div>1</div>';
         }
+        if($remainder > 0) {
+            $totalPages = (int)ceil($totalCount / $limit);
+            for($i = 2; $i <= $maxNextPage; $i++) {
+                $middle = sprintf('%s <div>%s</div>', $middle, $i);
+            }
+            if($totalPages > $maxNextPage) {
+                $last = sprintf('%s <div>%s</div>', $dots, $totalPages);
+            }
+        }
 
-        $remainder = $totalCount - $displayed;
+        // MIDDLE
+        // LAST
 
-        $elements = [$firstElement, $middle, $last]; 
-
-        return <<<HTML
-        <div>Displayed: {$displayed} </div>
-        <div>Total: {$totalCount}</div>
-        <div>Limit: {$limit}</div>
-        <div>Remainder: {$remainder} </div>    
-        HTML;
+        return sprintf('%s%s%s', $firstElement, $middle, $last);
+        // return <<<HTML
+        // <div>Displayed: {$displayed} </div>
+        // <div>Total: {$totalCount}</div>
+        // <div>Limit: {$limit}</div>
+        // <div>Remainder: {$remainder} </div>    
+        // HTML;
     }
 }
