a9ea80b72862685c6cd8c676c57edff74c28b2de
Romchik38 <pomahehko.c@gmail.com>
2024-11-14 19:33:19 +0200

    articticle view dto is ready

---
+++ app/code/Application/ArticleView/View/ArticleViewDTOFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Site2\Application\ArticleView\View;
+
+use Romchik38\Site2\Application\ArticleListView\View\DateFormatterInterface;
+
+final class ArticleViewDTOFactory
+{
+    protected const DATE_FORMAT_CATEGORY_PAGE = 'j-n-y';
+
+    public function __construct(
+        protected readonly DateFormatterInterface $dateFormatter
+    ) {}
+
+    public function create(
+        string $articleId,
+        string $name,
+        string $shortDescription,
+        string $description,
+        array $categories,
+        string $createdAt,
+        string $updatedAt
+    ): ArticleViewDTO {
+
+        $formattedCreatedAt = $this->dateFormatter->formatByString(
+            new \DateTime($createdAt),
+            $this::DATE_FORMAT_CATEGORY_PAGE
+        );
+
+        $formattedUpdatedAt = $this->dateFormatter->formatByString(
+            new \DateTime($updatedAt),
+            $this::DATE_FORMAT_CATEGORY_PAGE
+        );
+
+        return new ArticleViewDTO(
+            $articleId,
+            $name,
+            $shortDescription,
+            $description,
+            $categories,
+            $formattedCreatedAt,
+            $formattedUpdatedAt
+        );
+    }
+}
