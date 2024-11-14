d4ce3a3e8a46e32956a95e66df9ece178c7ecf36
Romchik38 <pomahehko.c@gmail.com>
2024-11-14 19:26:36 +0200

    Article view in progress

---
+++ app/code/Application/ArticleView/ArticleViewService.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Site2\Application\ArticleView;
+
+final class ArticleViewService {
+    
+}
---
+++ app/code/Application/ArticleView/View/ArticleViewDTO.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Site2\Application\ArticleView\View;
+
+/** Full info about Article */
+final class ArticleViewDTO
+{
+    public function __construct(
+        public readonly string $articleId,
+        public readonly string $name,
+        public readonly string $shortDescription,
+        public readonly string $description,
+        public readonly array $categories,
+        public readonly string $createdAt
+    ) {}
+
+}
