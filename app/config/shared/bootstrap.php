622c1bbfa2823f1040a3db4c4269ec35418a7581
Romchik38 <pomahehko.c@gmail.com>
2024-11-15 17:26:24 +0200

    added timezone `Europe/Kyiv` check

---
+++ app/config/shared/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use Romchik38\Server\Api\Services\DynamicRoot\DynamicRootInterface;
+
+return [
+    'date.timezone' => 'Europe/Kyiv',
+];
--- public/http/index.php
+++ public/http/index.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 require_once(__DIR__ . '/../../vendor/autoload.php');
 
+/** Checks */
+$bootstrapConfig = require_once(__DIR__ . '/../../app/config/shared/bootstrap.php');
+/** 1. timezone */
+$currentTimazone = ini_get('date.timezone');
+$expectedTimezone = $bootstrapConfig['date.timezone'];
+if ($expectedTimezone !== ini_get('date.timezone')) {
+    echo (sprintf('check timezone, expected %s, current %s', $expectedTimezone, $currentTimazone));
+    exit(1);
+}
+/** ? more checks */
+
+/** init app */
 $container = (require_once(__DIR__ . './../../app/bootstrap_http_sql.php'))();
 
-/** @var HttpServerInterface $server */
+
+/** run app */
 $server = $container->get(\Romchik38\Server\Api\Servers\Http\HttpServerInterface::class);
-
 $server->run()->log();
-
