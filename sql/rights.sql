4ca7fe9a16dc855fb84b05ec1f90ec8d9b16f813
Romchik38 <pomahehko.c@gmail.com>
2024-11-22 09:36:06 +0200

    fix

--- sql/rights.sql
+++ sql/rights.sql
@@ -1,22 +1,22 @@
-create role "service" LOGIN;
+create role "apache" LOGIN;
 
-GRANT connect on database site2 to "service";
+GRANT connect on database site2 to "apache";
 
-GRANT select, insert, update on table translate_entities to "service";
-GRANT select, insert, update on table translate_keys to "service";
-GRANT select, insert, update on table translate_lang to "service";
-GRANT select, insert, update on table links, links_translates to "service";
+GRANT select, insert, update on table translate_entities to "apache";
+GRANT select, insert, update on table translate_keys to "apache";
+GRANT select, insert, update on table translate_lang to "apache";
+GRANT select, insert, update on table links, links_translates to "apache";
 
 GRANT select, insert, update on table 
     article, article_translates, article_category 
-    to "service";
+    to "apache";
 
 GRANT select, insert, update on table 
     category, category_translates
-    to "service";
+    to "apache";
 
 GRANT select, insert, update on table 
-    persons, person_translates
-    to "service";
+    person, person_translates
+    to "apache";
 
-GRANT select, insert, update on table img, img_translates to "service";
+GRANT select, insert, update on table img, img_translates to "apache";
