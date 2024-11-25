13bff4c93b1f90de82923aebbf9c621e242e53b3
Romchik38 <pomahehko.c@gmail.com>
2024-11-25 18:37:56 +0200

    addet column created_at

--- sql/img_cache.sql
+++ sql/img_cache.sql
@@ -2,5 +2,6 @@ CREATE TABLE img_cache
 (
     key text NOT NULL PRIMARY KEY,
     data text NOT NULL,
-    type text NOT NULL CHECK ( type IN ('webp'))
+    type text NOT NULL CHECK ( type IN ('webp')),
+    created_at timestamp NOT NULL DEFAULT current_timestamp
 );
