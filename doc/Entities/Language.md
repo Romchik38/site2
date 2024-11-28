3f22a22f3e00b10078a341d47e9f78ccaad91611
Romchik38 <pomahehko.c@gmail.com>
2024-11-28 23:27:24 +0200

    refactored language

---
+++ doc/Entities/Language.md
@@ -0,0 +1,13 @@
+# Language
+
+- Language must be *activated*.
+- Requirements - All *active* entites must have translates:
+  - article
+  - audio
+  - author
+  - category
+  - img
+  - [?] link
+  - pereson
+  - translate_entities
+  
--- sql/translate_lang.sql
+++ sql/translate_lang.sql
@@ -1,8 +1,10 @@
 CREATE table translate_lang
 (
-    language text PRIMARY KEY
+    language text PRIMARY KEY,
+    active boolean NOT NULL DEFAULT false
 );
 
 INSERT INTO translate_lang (language) VALUES
-    ('en'), ('uk')
+    ('en'), 
+    ('uk')
 ;
