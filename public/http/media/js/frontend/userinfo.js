7c4849f1c2ab4dc385c7814ea9183ce346ac6c48
Romchik38 <pomahehko.c@gmail.com>
2025-03-10 19:33:02 +0200

    api userinfo point in progress

--- public/http/media/js/frontend/userinfo.js
+++ public/http/media/js/frontend/userinfo.js
@@ -7,15 +7,16 @@ var t = target(currentLanguage);
 var u = urlbuilder(t);
 
 addEventListener("DOMContentLoaded", (event) => {
+    var notloggedinElems = document.getElementsByClassName('header-user-notloggedin');
     var loggedinElems = document.getElementsByClassName('header-user-loggedin');
     var usernameElems = document.getElementsByClassName('user-name-field');
 
-    if (loggedinElems.length === 0 || usernameElems.length === 0) {
-        console.error('userinfo script started, but some of the elements not found on the page, pls check it: [header-user-loggedin, user-name-field]');
+    if (notloggedinElems.length === 0 || loggedinElems.length === 0 || usernameElems.length === 0) {
+        console.error('userinfo script started, but some of the elements not found on the page, pls check it: [header-user-loggedin, user-name-field, header-user-notloggedin]');
         return;
     }
 
-    var url = u (['root', 'admin', 'api', 'userinfo']);
+    var url = u (['root', 'api', 'userinfo']);
     const request = new Request(url, {
         method: "POST"
       });
@@ -34,6 +35,10 @@ addEventListener("DOMContentLoaded", (event) => {
                                 elem.innerText = result;
                             }
 
+                            for (var elem of notloggedinElems) {
+                                elem.style.display = 'none';
+                            }
+                            
                             for (var elem of loggedinElems) {
                                 elem.style.display = 'flex';
                             }
