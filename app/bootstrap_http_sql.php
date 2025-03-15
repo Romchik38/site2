e1dbb21b7bd786a015fa6ea5d955b507af76baf2
Romchik38 <pomahehko.c@gmail.com>
2025-03-15 19:01:29 +0200

    updated boostrap file

--- app/bootstrap_http_sql.php
+++ app/bootstrap_http_sql.php
@@ -19,14 +19,10 @@ return function (Container $container) {
     (require_once(__DIR__ . '/bootstrap/http/views.php'))($container);
     (require_once(__DIR__ . '/bootstrap/http/server.php'))($container);
     (require_once(__DIR__ . '/bootstrap/http/router.php'))($container);
-
-
-    /** 1st container get */
     (require_once(__DIR__  . '/bootstrap/actions.php'))($container);
+    
+    /** 1st container get */
     (require_once(__DIR__  . '/bootstrap/http/actionsList.php'))($container);
     
-
-
-
     return $container;
 };
