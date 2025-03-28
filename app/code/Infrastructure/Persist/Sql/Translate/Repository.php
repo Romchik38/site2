30e765d84daf1f50ebe25cb26988996bc7b8418d
Romchik38 <pomahehko.c@gmail.com>
2025-03-28 18:42:20 +0200

    feat(translate): repository is ready

--- app/code/Infrastructure/Persist/Sql/Translate/Repository.php
+++ app/code/Infrastructure/Persist/Sql/Translate/Repository.php
@@ -6,6 +6,7 @@ namespace Romchik38\Site2\Infrastructure\Persist\Sql\Translate;
 
 use Romchik38\Server\Models\Errors\QueryException;
 use Romchik38\Server\Models\Sql\DatabaseInterface;
+use Romchik38\Server\Models\Sql\DatabaseTransactionException;
 use Romchik38\Site2\Domain\Language\VO\Identifier as LanguageId;
 use Romchik38\Site2\Domain\Translate\RepositoryInterface;
 use Romchik38\Site2\Domain\Translate\RepositoryException;
@@ -62,19 +63,93 @@ final class Repository implements RepositoryInterface
     /** @throws CouldDeleteException */
     public function deleteById(Identifier $id): void
     {
-
+        $query = 'DELETE FROM translate_keys WHERE translate_keys.identifier = $1';
+        $params = [$id()];
+        try {
+            $this->database->queryParams($query, $params);
+        } catch (QueryException $e) {
+            throw new RepositoryException($e->getMessage());
+        }
     }
 
     /** @throws CouldNotSaveException */
     public function save(Translate $model): Translate
     {
-
+        $id = $model->getId();
+        $phrases = $model->getPhrases();
+        try {
+            $this->database->transactionStart();
+            $this->database->transactionQueryParams(
+                $this->deletePhrasesQuery(),
+                [$id()]
+            );
+            foreach ($phrases as $phrase) {
+                $this->database->transactionQueryParams(
+                    $this->phrasesSaveQueryInsert(),
+                    [
+                        $id(),
+                        (string) $phrase->getLanguage(),
+                        (string) $phrase->getPhrase(),
+                    ]
+                );
+            }
+            $this->database->transactionEnd();
+        } catch (DatabaseTransactionException $e) {
+            try {
+                $this->database->transactionRollback();
+                throw new CouldNotSaveException($e->getMessage());
+            } catch (DatabaseTransactionException $e2) {
+                throw new CouldNotSaveException($e2->getMessage());
+            }
+        } catch (QueryException $e) {
+            try {
+                $this->database->transactionRollback();
+                throw new CouldNotSaveException($e->getMessage());
+            } catch (DatabaseTransactionException $e2) {
+                throw new CouldNotSaveException($e2->getMessage());
+            }
+        }
+        return $this->getById($id);
     }
 
     /** @throws CouldNotSaveException */
     public function add(Translate $model): Translate
     {
-
+        $id = $model->getId();
+        $phrases = $model->getPhrases();
+        try {
+            $this->database->transactionStart();
+            $this->database->transactionQueryParams(
+                $this->insertTranslateQuery(),
+                [$id()]
+            );
+            foreach ($phrases as $phrase) {
+                $this->database->transactionQueryParams(
+                    $this->phrasesSaveQueryInsert(),
+                    [
+                        $id(),
+                        (string) $phrase->getLanguage(),
+                        (string) $phrase->getPhrase(),
+                    ]
+                );
+            }
+            $this->database->transactionEnd();
+        } catch (DatabaseTransactionException $e) {
+            try {
+                $this->database->transactionRollback();
+                throw new CouldNotSaveException($e->getMessage());
+            } catch (DatabaseTransactionException $e2) {
+                throw new CouldNotSaveException($e2->getMessage());
+            }
+        } catch (QueryException $e) {
+            try {
+                $this->database->transactionRollback();
+                throw new CouldNotSaveException($e->getMessage());
+            } catch (DatabaseTransactionException $e2) {
+                throw new CouldNotSaveException($e2->getMessage());
+            }
+        }
+        return $this->getById($id);
     }
 
     /** @return PhraseDto */
@@ -98,6 +173,14 @@ final class Repository implements RepositoryInterface
         }
     }
 
+    private function deletePhrasesQuery(): string
+    {
+        return <<<'QUERY'
+        DELETE FROM translate_entities
+        WHERE translate_entities.key = $1
+        QUERY;
+    }
+
     private function getByIdQuery(): string
     {
         return <<<'QUERY'
@@ -109,6 +192,22 @@ final class Repository implements RepositoryInterface
                 ON translate_keys.identifier = translate_entities.key
         WHERE translate_keys.identifier = $1
         ;
+        QUERY;
+    }
+
+    private function phrasesSaveQueryInsert(): string
+    {
+        return <<<'QUERY'
+        INSERT INTO translate_entities (key, language, phrase)
+        VALUES ($1, $2, $3)
+        QUERY;
+    }
+
+    private function insertTranslateQuery(): string
+    {
+        return <<<'QUERY'
+        INSERT INTO translate_keys (identifier)
+        VALUES ($1)
         QUERY;
     }
 }
