5aed919d368380956a32204fd2cb9a9c1ab392fc
Romchik38 <pomahehko.c@gmail.com>
2025-04-04 17:27:21 +0300

    feat(image): admin view template looks googd

--- app/code/Infrastructure/Controllers/Actions/GET/Admin/Image/DynamicAction.php
+++ app/code/Infrastructure/Controllers/Actions/GET/Admin/Image/DynamicAction.php
@@ -89,6 +89,7 @@ final class DynamicAction extends AbstractMultiLanguageAction implements Dynamic
             Update::NAME_FIELD,
             Update::AUTHOR_ID_FIELD,
             Update::CHANGE_AUTHOR_ID_FIELD,
+            Update::CHANGE_AUTHOR_FIELD,
             Update::CHANGE_ACTIVITY_FIELD,
             Update::CHANGE_ACTIVITY_YES_FIELD,
             Update::CHANGE_ACTIVITY_NO_FIELD,
