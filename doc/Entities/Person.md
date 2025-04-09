3a174e991da46241a92f208218a45dfb441d8226
Romchik38 <pomahehko.c@gmail.com>
2025-04-09 17:44:00 +0300

    refactor(sql): removed table person

--- doc/Entities/Person.md
+++
@@ -1,9 +0,0 @@
-# Person
-
-Represents a Person.
-
-- Person must be *activated*.
-- Requirements
-  - all translates
-- Cannot be *deactivated* if has referencies from:
-  - no references for now
--- sql/rights.sql
+++ sql/rights.sql
@@ -7,7 +7,6 @@ GRANT select, insert, update on table language to "apache";
 GRANT select, insert, update on table links, links_translates to "apache";
 GRANT select, insert, update on table article, article_translates, article_category to "apache";
 GRANT select, insert, update on table category, category_translates to "apache";
-GRANT select, insert, update on table person, person_translates to "apache";
 GRANT select, insert, update on table img, img_translates to "apache";
 GRANT select, insert, update on table img_cache to "apache";
 GRANT select, insert, update on table author, author_translates to "apache";
