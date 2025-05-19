50c7aabd6334992b86213a95bf3d774b71fe50e2
Romchik38 <pomahehko.c@gmail.com>
2025-05-19 17:57:02 +0300

    test(article): method __construct

--- app/code/Domain/Article/Entities/Category.php
+++ app/code/Domain/Article/Entities/Category.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace Romchik38\Site2\Domain\Article\Entities;
 
-use Romchik38\Site2\Domain\Article\VO\Identifier as ArticleId;
 use Romchik38\Site2\Domain\Category\VO\Identifier as CategoryId;
 
 final class Category
 {
     public function __construct(
-        public readonly ArticleId $articleId,
         public readonly CategoryId $categoryId,
+        public readonly bool $active
     ) {
     }
 }
--- tests/Unit/Domain/Article/Entities/ArticleTest.php
+++ tests/Unit/Domain/Article/Entities/ArticleTest.php
@@ -7,13 +7,19 @@ namespace Romchik38\Tests\Unit\Domain\Article\Entities;
 use DateTime;
 use PHPUnit\Framework\TestCase;
 use Romchik38\Site2\Domain\Article\Article;
+use Romchik38\Site2\Domain\Article\Entities\Audio;
 use Romchik38\Site2\Domain\Article\Entities\Author;
+use Romchik38\Site2\Domain\Article\Entities\Category;
+use Romchik38\Site2\Domain\Article\Entities\Image;
 use Romchik38\Site2\Domain\Article\Entities\Translate;
 use Romchik38\Site2\Domain\Article\VO\Description;
 use Romchik38\Site2\Domain\Article\VO\Identifier as ArticleId;
 use Romchik38\Site2\Domain\Article\VO\Name;
 use Romchik38\Site2\Domain\Article\VO\ShortDescription;
+use Romchik38\Site2\Domain\Audio\VO\Id as AudioId;
 use Romchik38\Site2\Domain\Author\VO\AuthorId;
+use Romchik38\Site2\Domain\Category\VO\Identifier as CategoryId;
+use Romchik38\Site2\Domain\Image\VO\Id as ImageId;
 use Romchik38\Site2\Domain\Language\VO\Identifier as LanguageId;
 
 final class ArticleTest extends TestCase
@@ -81,5 +87,113 @@ final class ArticleTest extends TestCase
         $this->assertSame($translates, $article->getTranslates());
     }
 
+    public function testCreateWithImage(): void
+    {
+        $id        = new ArticleId('some-id');
+        $author    = new Author(new AuthorId('1'), true);
+        $languages = [
+            new LanguageId('en'),
+            new LanguageId('uk'),
+        ];
 
+        $article = Article::create(
+            $id,
+            $author,
+            $languages
+        );
+
+        $image = new Image(new ImageId(1), true);
+
+        $article = Article::create(
+            $id,
+            $author,
+            $languages,
+            null,
+            $image
+        );
+
+        $this->assertSame($image, $article->image);
+    }
+
+    public function testCreateWithAudio(): void
+    {
+        $id        = new ArticleId('some-id');
+        $author    = new Author(new AuthorId('1'), true);
+        $languages = [
+            new LanguageId('en'),
+            new LanguageId('uk'),
+        ];
+
+        $article = Article::create(
+            $id,
+            $author,
+            $languages
+        );
+
+        $audio = new Audio(new AudioId(1), true);
+
+        $article = Article::create(
+            $id,
+            $author,
+            $languages,
+            $audio
+        );
+
+        $this->assertSame($audio, $article->audio);
+    }
+
+    public function testConstractValidArticle(): void
+    {
+        $id     = new ArticleId('some-id');
+        $audio  = new Audio(new AudioId(1), true);
+        $author = new Author(new AuthorId('1'), true);
+        $image  = new Image(new ImageId(1), true);
+
+        $categories = [
+            new Category(new CategoryId('cat-1'), true),
+        ];
+
+        $languages = [
+            new LanguageId('en'),
+            new LanguageId('uk'),
+        ];
+
+        $translates = [
+            new Translate(
+                new LanguageId('en'),
+                new Name('some name'),
+                new ShortDescription('Some article short description'),
+                new Description('Some article description'),
+                new DateTime(),
+                new DateTime()
+            ),
+            new Translate(
+                new LanguageId('uk'),
+                new Name('Стаття про щось'),
+                new ShortDescription('Короткий опис статті про щось'),
+                new Description('Повний опис статті про щось'),
+                new DateTime(),
+                new DateTime()
+            ),
+        ];
+
+        $article = new Article(
+            $id,
+            true,
+            $audio,
+            $author,
+            $image,
+            $categories,
+            $languages,
+            $translates
+        );
+
+        $this->assertSame($id, $article->id);
+        $this->assertSame(true, $article->active);
+        $this->assertSame($audio, $article->audio);
+        $this->assertSame($author, $article->author);
+        $this->assertSame($image, $article->image);
+        $this->assertSame($categories, $article->getCategories());
+        $this->assertSame($translates, $article->getTranslates());
+    }
 }
