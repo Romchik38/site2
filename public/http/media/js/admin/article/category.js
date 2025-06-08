849fb7207e5cb69a8010675ca504848926c20ab2
Romchik38 <pomahehko.c@gmail.com>
2025-06-08 16:42:27 +0300

    feat(article): admintemplate category - created hint component

--- public/http/media/js/admin/article/category.js
+++ public/http/media/js/admin/article/category.js
@@ -4,6 +4,7 @@ import { default as Component } from '/media/js/modules/components/component.js'
 
 var bc = Component.fromClass('category-button-change');
 var s = Component.fromClass('category-select');
+var h = Component.fromClass('category-hint');
 
 /* Category Components:
     category-button-change              bc              button change
@@ -15,10 +16,12 @@ var isSelectOpen = false;
 bc.onEvent('click', () => {
     if (isSelectOpen === false) {
         s.show();
+        h.show();
         bc.text('Hide');
         isSelectOpen = true;
     } else {
         s.hide();
+        h.hide();
         bc.text('Change');
         isSelectOpen = false;
     }
