6fd838b3b92885e50fb3dc58bf779b2802f525f7
Romchik38 <pomahehko.c@gmail.com>
2025-06-11 19:44:05 +0300

    refactor(sql): structure

--- sql/category.sql
+++ sql/category.sql
@@ -1,16 +1,16 @@
 CREATE table category
 (
-        identifier text UNIQUE NOT NULL,
-        active boolean NOT NULL DEFAULT false
-    );
+    identifier text UNIQUE NOT NULL,
+    active boolean NOT NULL DEFAULT false
+);
 
-CREATE table
-    category_translates (
-        category_id text NOT NULL REFERENCES category (identifier) ON UPDATE CASCADE ON DELETE CASCADE,
-        language text NOT NULL REFERENCES language (identifier) ON UPDATE CASCADE,
-        name text NOT NULL UNIQUE,
-        description text NOT NULL,
-        CONSTRAINT pk_category_translates PRIMARY KEY (category_id, language)
-    );
+CREATE table category_translates 
+(
+    category_id text NOT NULL REFERENCES category (identifier) ON UPDATE CASCADE ON DELETE CASCADE,
+    language text NOT NULL REFERENCES language (identifier) ON UPDATE CASCADE,
+    name text NOT NULL UNIQUE,
+    description text NOT NULL,
+    CONSTRAINT pk_category_translates PRIMARY KEY (category_id, language)
+);
 
 
--- sql/img.sql
+++ sql/img.sql
@@ -1,19 +1,19 @@
-CREATE table
-    img (
-        identifier serial NOT NULL PRIMARY KEY,
-        active boolean NOT NULL DEFAULT false,
-        name text not NULL,
-        author_id int NOT NULL REFERENCES author (identifier) ON UPDATE CASCADE,
-        path text NOT NULL
-    );
+CREATE table img 
+(
+    identifier serial NOT NULL PRIMARY KEY,
+    active boolean NOT NULL DEFAULT false,
+    name text not NULL,
+    author_id int NOT NULL REFERENCES author (identifier) ON UPDATE CASCADE,
+    path text NOT NULL
+);
 
-CREATE table
-    img_translates (
-        img_id int NOT NULL REFERENCES img (identifier) ON UPDATE CASCADE ON DELETE CASCADE,
-        language text NOT NULL REFERENCES language (identifier) ON UPDATE CASCADE ON DELETE CASCADE,
-        description text NOT NULL,
-        CONSTRAINT pk_img_translates PRIMARY KEY (img_id, language)
-    );
+CREATE table img_translates 
+(
+    img_id int NOT NULL REFERENCES img (identifier) ON UPDATE CASCADE ON DELETE CASCADE,
+    language text NOT NULL REFERENCES language (identifier) ON UPDATE CASCADE ON DELETE CASCADE,
+    description text NOT NULL,
+    CONSTRAINT pk_img_translates PRIMARY KEY (img_id, language)
+);
 
 INSERT INTO img (identifier, name, author_id, path) VALUES
     (1, 'simplification-of-the-drivers-license-examination-process', 1, 'articles/simplification-of-the-drivers-license-examination-process/1.webp')
