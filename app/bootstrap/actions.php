d4914876945fc3d6fd8a8b72e66481e15b66296c
Romchik38 <pomahehko.c@gmail.com>
2025-06-12 18:21:14 +0300

    refactor(view): replaced twig image

--- app/bootstrap/actions.php
+++ app/bootstrap/actions.php
@@ -284,7 +284,7 @@ return function (Container $container) {
         [
             new Promise('\Romchik38\Server\Http\Routers\Handlers\DynamicRoot\DynamicRootInterface'),
             new Promise('\Romchik38\Server\Utils\Translate\TranslateInterface'),
-            new Promise('admin_view'),
+            new Promise('admin_view_layout'),
             new Promise('\Romchik38\Site2\Application\Image\AdminImageListService\AdminImageListService'),
             new Promise('\Romchik38\Server\Http\Utils\Urlbuilder\UrlbuilderInterface'),
             new Promise('\Romchik38\Site2\Infrastructure\Http\Services\Session\Site2SessionInterface'),
@@ -297,7 +297,7 @@ return function (Container $container) {
         [
             new Promise('\Romchik38\Server\Http\Routers\Handlers\DynamicRoot\DynamicRootInterface'),
             new Promise('\Romchik38\Server\Utils\Translate\TranslateInterface'),
-            new Promise('admin_view'),
+            new Promise('admin_view_layout'),
             new Promise('\Romchik38\Site2\Application\Image\AdminView\AdminViewService'),
             new Promise('\Romchik38\Site2\Infrastructure\Http\Services\Session\Site2SessionInterface'),
             new Promise('\Romchik38\Site2\Infrastructure\Utils\TokenGenerators\CsrfTokenGeneratorInterface'),
@@ -313,7 +313,7 @@ return function (Container $container) {
         [
             new Promise('\Romchik38\Server\Http\Routers\Handlers\DynamicRoot\DynamicRootInterface'),
             new Promise('\Romchik38\Server\Utils\Translate\TranslateInterface'),
-            new Promise('admin_view'),
+            new Promise('admin_view_layout'),
             new Promise('\Romchik38\Site2\Infrastructure\Http\Services\Session\Site2SessionInterface'),
             new Promise('\Romchik38\Site2\Infrastructure\Utils\TokenGenerators\CsrfTokenGeneratorInterface'),
             new Promise('\Romchik38\Site2\Application\Language\List\ListService'),
