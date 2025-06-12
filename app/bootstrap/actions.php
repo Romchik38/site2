8f4e7d0c08e9334cc14a296558a522aff67dc945
Romchik38 <pomahehko.c@gmail.com>
2025-06-12 18:05:58 +0300

    refactor(view): replaced twig category new

--- app/bootstrap/actions.php
+++ app/bootstrap/actions.php
@@ -253,7 +253,7 @@ return function (Container $container) {
         [
             new Promise('\Romchik38\Server\Http\Routers\Handlers\DynamicRoot\DynamicRootInterface'),
             new Promise('\Romchik38\Server\Utils\Translate\TranslateInterface'),
-            new Promise('admin_view'),
+            new Promise('admin_view_layout'),
             new Promise('\Romchik38\Site2\Infrastructure\Http\Services\Session\Site2SessionInterface'),
             new Promise('\Romchik38\Site2\Infrastructure\Utils\TokenGenerators\CsrfTokenGeneratorInterface'),
             new Promise('\Romchik38\Site2\Application\Language\List\ListService'),
