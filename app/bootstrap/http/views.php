fc4bb4d8ecd11ffe5632aa901165df62662c330e
Romchik38 <pomahehko.c@gmail.com>
2025-06-19 18:16:34 +0300

    fix(bootstrap): added image path to admin view

--- app/bootstrap/http/views.php
+++ app/bootstrap/http/views.php
@@ -52,6 +52,7 @@ return function (Container $container) {
             new Promise('\Romchik38\Server\Http\Controller\Mappers\Breadcrumb\Breadcrumb'),
             new Promise('\Romchik38\Server\Http\Utils\Urlbuilder\UrlbuilderInterface'),
             new Promise('\Romchik38\Site2\Infrastructure\Http\Services\Session\Site2SessionInterface'),
+            new Promise('img-folder-frontend'),
             'admin_layout'
         ]
     );
