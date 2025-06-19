6bcbd3a8f053d4cf4a9972070bd6c7d037b26dce
Romchik38 <pomahehko.c@gmail.com>
2025-06-19 14:23:17 +0300

    feat(banner): view repository is ready

---
+++ app/code/Infrastructure/Persist/Sql/ReadModels/Banner/List/Repository.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Site2\Infrastructure\Persist\Sql\ReadModels\Banner\AdminList;
+
+use InvalidArgumentException;
+use Romchik38\Server\Persist\Sql\DatabaseSqlInterface;
+use Romchik38\Server\Persist\Sql\QueryException;
+use Romchik38\Site2\Application\Banner\List\Exceptions\RepositoryException;
+use Romchik38\Site2\Application\Banner\List\RepositoryInterface;
+use Romchik38\Site2\Application\Banner\List\View\BannerDto;
+use Romchik38\Site2\Domain\Banner\VO\Identifier as BannerId;
+use Romchik38\Site2\Domain\Banner\VO\Name;
+use Romchik38\Site2\Domain\Image\VO\Id as ImageId;
+
+final class Repository implements RepositoryInterface
+{
+    public function __construct(
+        private readonly DatabaseSqlInterface $database
+    ) {
+    }
+
+    public function list(): array
+    {
+        $query = $this->defaultQuery();
+        try {
+            $rows = $this->database->queryParams($query, []);
+        } catch (QueryException $e) {
+            throw new RepositoryException($e->getMessage());
+        }
+
+        $models = [];
+        foreach ($rows as $row) {
+            $models[] = $this->createFromRow($row);
+        }
+        return $models;
+    }
+
+    /**
+     * @throws RepositoryException
+     * @param array<string,string|null> $row
+     * */
+    private function createFromRow(array $row): BannerDto
+    {
+        $rawIdentifier = $row['identifier'] ?? null;
+        if ($rawIdentifier === null) {
+            throw new RepositoryException('Banner id is invalid');
+        }
+
+        $rawName = $row['name'] ?? null;
+        if ($rawName === null) {
+            throw new RepositoryException('Banner name is invalid');
+        }
+
+        $rawImageIdentifier = $row['img_id'] ?? null;
+        if ($rawImageIdentifier === null) {
+            throw new RepositoryException('Banner image id is invalid');
+        }
+
+        try {
+            $id      = BannerId::fromString($rawIdentifier);
+            $name    = new Name($rawName);
+            $imageId = ImageId::fromString($rawImageIdentifier);
+        } catch (InvalidArgumentException $e) {
+            throw new RepositoryException($e->getMessage());
+        }
+
+        return new BannerDto($id, $name, $imageId);
+    }
+
+    private function defaultQuery(): string
+    {
+        return <<<QUERY
+        SELECT banner.identifier,
+            banner.name,
+            banner.img_id
+        FROM banner
+        WHERE banner.active = 't',
+        QUERY;
+    }
+}
