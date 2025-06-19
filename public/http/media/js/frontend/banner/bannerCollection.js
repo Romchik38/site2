dbe11325b0c4b618de2d1743f88d11d84512190d
Romchik38 <pomahehko.c@gmail.com>
2025-06-19 18:08:00 +0300

    feat(banner): created bannerCollection

---
+++ public/http/media/js/frontend/banner/bannerCollection.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import { default as Component } from '/media/js/modules/components/component.js';
+
+class BannerCollection {
+    constructor(nodes) {
+        if (! nodes instanceof HTMLCollection ) {
+            throw new Error('Param nodes is invalid. Expected HTMLCollection');
+        }
+        this.nodes = [];
+        for (var node of nodes) {
+            this.nodes.push(new Component(node));
+        }
+        
+    }
+
+    static fromClass(className) {
+        if (typeof className !== 'string') {
+            throw new Error('Param className is invalid');
+        }
+        return new this(document.getElementsByClassName(className));
+    }
+
+    start() {
+        var first = this.nodes[0];
+        first.show();
+    }
+};
+
+export default BannerCollection;
