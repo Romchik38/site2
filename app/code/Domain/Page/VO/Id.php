1f036d363ce02d33b0deaa725739711c484d9317
Romchik38 <pomahehko.c@gmail.com>
2025-07-13 16:42:29 +0300

    feat(page): created id vo

---
+++ app/code/Domain/Page/VO/Id.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Romchik38\Site2\Domain\Page\VO;
+
+use Romchik38\Server\Domain\VO\Number\Positive;
+
+final class Id extends Positive
+{
+    public const NAME = 'Page Id';
+}
