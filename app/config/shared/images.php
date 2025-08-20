4881f8781aaa116142213f3915883f31e596a385
Romchik38 <pomahehko.c@gmail.com>
2025-08-20 17:54:30 +0300

    feat(image cache): added max cache size

--- app/config/shared/images.php
+++ app/config/shared/images.php
@@ -9,6 +9,7 @@ return [
     /** 
      * Path to public html img folder
      * */
+    'img-max-cache-size' => 1_000_000, // 1MB,
     'img-folder-frontend' => '/media/img',
-    'img-folder-backend' => __DIR__ . '/../../../public/http/media/img'
+    'img-folder-backend' => __DIR__ . '/../../../public/http/media/img',
 ];
